be294a0c6e95319fc98395b3a03a5b4a79019e99
rinkurajput25803-creator <rinkurajput25803@gmail.com>
2026-03-16 14:26:20 +0530

    Add logging functionality to logger.py

---
+++ utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig(
+    filename="system.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
+
+def log_detection(status):
+
+    logging.info(f"Detection Result: {status}")
